f53d3591ddf70d637fab6d7b45a264c6d9fb96da
894492449 <894492449@qq.com>
2017-07-06 17:11:00 +0800

    修改

--- test1.js
+++ test1.js
@@ -1,6 +1,1 @@
-var http=require("http");
-http.createServer(function(request,response){
-	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.end('hello world\n');
-}).listen(8888);
-console.log('Server running at http://')
+var a="修改了第一次"
